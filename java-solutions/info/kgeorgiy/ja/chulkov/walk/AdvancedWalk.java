636827f0202a46320113005d4664774572a2f7a9
mralex1810 <sash39av@gmail.com>
2023-02-17 15:23:46 +0300

    exception messages

--- java-solutions/info/kgeorgiy/ja/chulkov/walk/AdvancedWalk.java
+++ java-solutions/info/kgeorgiy/ja/chulkov/walk/AdvancedWalk.java
@@ -1,6 +1,8 @@
 package info.kgeorgiy.ja.chulkov.walk;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.*;
 import java.util.EnumSet;
 
@@ -32,57 +34,51 @@ public class AdvancedWalk {
             return;
         }
         try {
-            Path inputFile = Path.of(args[0]);
+            Path outputFile = Path.of(args[1]);
             try {
-                Path outputFile = Path.of(args[1]);
-                try {
-                    if (outputFile.getParent() == null) {
-                        System.err.println("Can't create parent dirs of output files");
-                        return;
-                    }
-                    if (!Files.exists(outputFile.getParent())) {
-                        Files.createDirectories(outputFile.getParent());
-                    }
-                } catch (IOException e) {
-                    System.err.println("Can't create parent dirs of output files");
-                    return;
+                if (outputFile.getParent() != null && !Files.exists(outputFile.getParent())) {
+                    Files.createDirectories(outputFile.getParent());
                 }
-                try (BufferedReader in = Files.newBufferedReader(inputFile)) {
-                    try (Writer out = Files.newBufferedWriter(outputFile);
-                         HashResultsHandler handler = new HashResultsHandler(out)) {
-                        try {
-                            FileVisitor<Path> visitor = new HashFileVisitor<>(handler);
-                            for (String file = in.readLine(); file != null; file = in.readLine()) {
-                                walkFromOneFile(file, depth, visitor, handler);
-                            }
-                        } catch (IOException e) {
-                            System.err.println("Error on reading input file");
+            } catch (IOException e) {
+                System.err.println("Can't create parent dirs of output files " + e.getMessage());
+                return;
+            }
+            try (BufferedReader in = Files.newBufferedReader(Path.of(args[0]))) {
+                try (Writer out = Files.newBufferedWriter(outputFile);
+                     HashResultsHandler handler = new HashResultsHandler(out)) {
+                    try {
+                        FileVisitor<Path> visitor = new HashFileVisitor<>(handler);
+                        for (String file = in.readLine(); file != null; file = in.readLine()) {
+                            walkFromOneFile(file, depth, visitor, handler);
                         }
                     } catch (IOException e) {
-                        System.err.println("Error on open output file");
+                        System.err.println("Error on reading input file " + e.getMessage());
+                    } catch (InvalidPathException e) {
+                        System.err.println("Input file string isn't path " + e.getReason());
                     }
                 } catch (IOException e) {
-                    System.err.println("Error on open input file");
+                    System.err.println("Error on open output file " + e.getMessage());
                 }
-            } catch (InvalidPathException e) {
-                System.err.println("Output file string isn't path");
+            } catch (IOException e) {
+                System.err.println("Error on open input file " + e.getMessage());
             }
         } catch (InvalidPathException e) {
-            System.err.println("Input file string isn't path");
+            System.err.println("Output file string isn't path: " + e.getReason());
         }
     }
+
 
     private static void walkFromOneFile(String file, int depth, FileVisitor<Path> visitor, HashResultsHandler handler) {
         try {
             Files.walkFileTree(Path.of(file), EnumSet.noneOf(FileVisitOption.class), depth, visitor);
         } catch (IOException e) {
             // Unreachable
-            System.err.println("Error on processing file " + file);
+            System.err.println("Error on processing file " + file + " : " + e.getMessage());
         } catch (InvalidPathException e) {
-            System.err.println("Error on getting path of " + file);
+            System.err.println("Error on getting path of " + file  + " : " + e.getReason());
             handler.processError(file);
         } catch (SecurityException e) {
-            System.err.println("Security manager denies access to " + file);
+            System.err.println("Security manager denies access to " + file  + " : " + e.getMessage());
             handler.processError(file);
         }
 
