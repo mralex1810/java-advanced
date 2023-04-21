f46cea49764f67f3668db46554b3954fb2cdf35e
mralex1810 <sash39av@gmail.com>
2023-04-21 18:13:28 +0300

    8 fix

--- java-solutions/info/kgeorgiy/ja/chulkov/concurrent/ParallelMapperImpl.java
+++ java-solutions/info/kgeorgiy/ja/chulkov/concurrent/ParallelMapperImpl.java
@@ -13,7 +13,8 @@ import java.util.stream.IntStream;
  * Implementation of {@link ParallelMapper}
  */
 public class ParallelMapperImpl implements ParallelMapper {
-    private final SynchronizedQueue tasks = new SynchronizedQueue();
+    private final SynchronizedQueue<Task<?>> tasks = new SynchronizedQueue<>();
+    private volatile boolean closed = false;
 
     private final List<Thread> threads;
 
@@ -28,42 +29,50 @@ public class ParallelMapperImpl implements ParallelMapper {
         final Runnable worker = () -> {
             try {
                 while (!Thread.interrupted()) {
-                    tasks.take().run();
+                    tasks.take().runnable.run();
                 }
             } catch (final InterruptedException ignored) {
             }
         };
 
         // :NOTE: (Int)Stream
-        this.threads = new ArrayList<>(threadsNum);
-        for (int i = 0; i < threadsNum; i++) {
-            threads.add(new Thread(worker));
-            threads.get(i).start();
-        }
+        this.threads = IntStream.range(0, threadsNum)
+                .mapToObj(it -> new Thread(worker))
+                .peek(Thread::start)
+                .toList();
     }
 
     public <T, R> List<R> map(
             final Function<? super T, ? extends R> f,
             final List<? extends T> args
     ) throws InterruptedException {
+        checkClosed();
         final Results<R> results = new Results<>(args.size());
-        synchronized (tasks) {
-            IntStream.range(0, args.size())
-                    .forEach(index -> tasks.put(() -> {
-                        try {
-                            results.setResult(index, f.apply(args.get(index)));
-                        } catch (final RuntimeException e) {
-                            // :NOTE: RuntimeException
-                            results.setException(new RuntimeException("Error on mapping", e));
-                        }
-                    }));
-        }
+        tasks.putAll(IntStream.range(0, args.size())
+                .<Task<?>>mapToObj(index -> new Task<>(() -> {
+                    try {
+                        results.setResult(index, f.apply(args.get(index)));
+                    } catch (final RuntimeException e) {
+                        // :NOTE: RuntimeException
+                        results.setException(e);
+                    }
+                }, results))
+                .toList()
+        );
         return results.getResults();
+    }
+
+    private void checkClosed() {
+        if (closed) {
+            throw new IllegalStateException("Mapper was closed");
+        }
     }
 
     @Override
     public void close() {
         // :NOTE: hang map
+        closed = true;
+        tasks.queue.forEach(it -> it.result.notify());
         threads.forEach(Thread::interrupt);
         for (final Thread thread : threads) {
             boolean joined = false;
@@ -77,30 +86,36 @@ public class ParallelMapperImpl implements ParallelMapper {
         }
     }
 
-    private static class SynchronizedQueue {
+    private record Task<R>(Runnable runnable, Results<R> result) {}
 
-        private final Queue<Runnable> tasksQueue = new ArrayDeque<>();
+    private static class SynchronizedQueue<T> {
 
-        private void put(final Runnable task) {
-            tasksQueue.add(task);
+        private final Queue<T> queue = new ArrayDeque<>();
+
+        private void put(final T obj) {
+            queue.add(obj);
             notify();
         }
 
-        private synchronized Runnable take() throws InterruptedException {
-            while (tasksQueue.isEmpty()) {
+        private synchronized void putAll(final List<T> list) {
+            list.forEach(this::put);
+        }
+
+        private synchronized T take() throws InterruptedException {
+            while (queue.isEmpty()) {
                 wait();
             }
-            return tasksQueue.poll();
+            return queue.poll();
         }
     }
 
-    private static class Results<R> {
-
+    private class Results<R>  {
         private final List<R> results;
-        private int resultsCount;
+        private int resultsCountRest;
         private RuntimeException exception = null;
 
         private Results(final int size) {
+            resultsCountRest = size;
             results = new ArrayList<>(Collections.nCopies(size, null));
         }
 
@@ -119,16 +134,17 @@ public class ParallelMapperImpl implements ParallelMapper {
         }
 
         private void incrementResultCounter() {
-            resultsCount++; // :NOTE: -> 0
-            if (resultsCount == results.size()) {
+            resultsCountRest--; // :NOTE: -> 0
+            if (resultsCountRest == 0) {
                 notify();
             }
         }
 
         synchronized List<R> getResults() throws InterruptedException {
-            while (resultsCount < results.size()) {
+            while (resultsCountRest != 0 && !closed) {
                 wait();
             }
+            checkClosed();
             if (exception != null) {
                 throw exception;
             }
