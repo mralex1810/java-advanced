73b53c7d43e95d87991c185c702ba13f0687b8f4
mralex1810 <sash39av@gmail.com>
2023-05-04 23:41:25 +0300

    10  works

--- java-solutions/info/kgeorgiy/ja/chulkov/hello/HelloUDPServer.java
+++ java-solutions/info/kgeorgiy/ja/chulkov/hello/HelloUDPServer.java
@@ -6,73 +6,84 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.IntStream;
 
 public class HelloUDPServer implements HelloServer {
 
     public static final int LENGTH_LIMIT = 1024;
-    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
     private final Function<DatagramPacket, Supplier<byte[]>> taskGenerator = (it) -> () ->
             String.format("Hello, %s", HelloUDPClient.getDecodedData(it)).getBytes(StandardCharsets.UTF_8);
+    private final Semaphore semaphore = new Semaphore(1024);
+    private Thread getterThread;
+    private ExecutorService taskExecutorService;
     private DatagramSocket datagramSocket;
-    private List<Thread> threadsList;
 
     @Override
     public void start(final int port, final int threads) {
+        taskExecutorService = Executors.newFixedThreadPool(threads);
         try {
             datagramSocket = new DatagramSocket(port);
         } catch (final SocketException e) {
             throw new RuntimeException(e);
         }
-        final Runnable task = () -> {
+        getterThread = new Thread(() -> {
             while (!Thread.interrupted()) {
                 try {
                     final var datagramPacketForReceive = new DatagramPacket(new byte[LENGTH_LIMIT], LENGTH_LIMIT);
                     datagramSocket.receive(datagramPacketForReceive);
-                    CompletableFuture.supplyAsync(taskGenerator.apply(datagramPacketForReceive))
-                            .thenAcceptAsync((ans) -> {
-                                        final var datagramPacketToSend = new DatagramPacket(
-                                                ans,
-                                                ans.length,
-                                                datagramPacketForReceive.getAddress(),
-                                                datagramPacketForReceive.getPort());
-                                        try {
-                                            datagramSocket.send(datagramPacketToSend);
-                                        } catch (final IOException e) {
-                                            throw new RuntimeException(e);
-                                        }
+                    semaphore.acquire();
+                    CompletableFuture
+                            .supplyAsync(taskGenerator.apply(datagramPacketForReceive), taskExecutorService)
+                            .thenAccept((ans) -> {
+                                try {
+                                    final var datagramPacketToSend = new DatagramPacket(
+                                            ans,
+                                            ans.length,
+                                            datagramPacketForReceive.getAddress(),
+                                            datagramPacketForReceive.getPort());
+                                    try {
+                                        datagramSocket.send(datagramPacketToSend);
+                                    } catch (final IOException e) {
+                                        System.err.println("Error on executing task");
                                     }
-                            ).get();
-                } catch (final IOException | InterruptedException | ExecutionException e) {
+                                } finally {
+                                    semaphore.release();
+                                }
+                            })
+                            .handle((ans, e) -> {
+                                semaphore.release();
+                                if (e != null) {
+                                    System.err.println("Error on executing task " + e.getMessage());
+                                }
+                                return ans;
+                            });
+                } catch (final IOException | InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
-        };
-        threadsList = IntStream.range(0, threads)
-                .mapToObj(it -> new Thread(task))
-                .peek(Thread::start)
-                .toList();
+        });
+        getterThread.start();
     }
 
     @Override
     public void close() {
-        threadsList.forEach(Thread::interrupt);
+        getterThread.interrupt();
+        taskExecutorService.shutdownNow();
         datagramSocket.close();
-        threadsList.forEach(it -> {
-            while (true) {
-                try {
-                    it.join();
+        while (true) {
+            try {
+                getterThread.join();
+                if (taskExecutorService.awaitTermination(1, TimeUnit.DAYS)) {
                     break;
-                } catch (final InterruptedException ignored) {
                 }
+            } catch (final InterruptedException ignored) {
             }
-        });
+        }
     }
 }
