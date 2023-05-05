60235755983043d750eb989b1df8f7f5cf2a41ad
mralex1810 <sash39av@gmail.com>
2023-05-05 15:14:09 +0300

    10 bengal update
    Took 55 minutes

--- java-solutions/info/kgeorgiy/ja/chulkov/hello/HelloUDPClient.java
+++ java-solutions/info/kgeorgiy/ja/chulkov/hello/HelloUDPClient.java
@@ -10,12 +10,12 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
-import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 /**
@@ -23,19 +23,7 @@ import java.util.stream.IntStream;
  */
 public class HelloUDPClient implements HelloClient {
 
-    private static final int TIMEOUT = 100;
-    private static final List<String> ANSWERS =
-            List.of("Hello, %s", "%s ආයුබෝවන්", "Բարեւ, %s", "مرحبا %s", "Салом %s", "Здраво %s", "Здравейте %s",
-                    "Прывітанне %s", "Привіт %s", "Привет, %s", "Поздрав %s", "سلام به %s", "שלום %s", "Γεια σας %s",
-                    "העלא %s", "ہیل%s٪ ے", "Bonjou %s", "Bonjour %s", "Bună ziua %s", "Ciao %s", "Dia duit %s",
-                    "Dobrý deň %s", "Dobrý den, %s", "Habari %s", "Halló %s", "Hallo %s", "Halo %s", "Hei %s", "Hej %s",
-                    "Hello  %s", "Hello %s", "Hello %s", "Helo %s", "Hola %s", "Kaixo %s", "Kamusta %s", "Merhaba %s",
-                    "Olá %s", "Ola %s", "Përshëndetje %s", "Pozdrav %s", "Pozdravljeni %s", "Salom %s", "Sawubona %s",
-                    "Sveiki %s", "Tere %s", "Witaj %s", "Xin chào %s", "ສະບາຍດີ %s", "สวัสดี %s", "ഹലോ %s", "ಹಲೋ %s",
-                    "హలో %s", "हॅलो %s", "नमस्कार%sको", "হ্যালো %s", "ਹੈਲੋ %s", "હેલો %s", "வணக்கம் %s",
-                    "ကို %s မင်္ဂလာပါ", "გამარჯობა %s", "ជំរាបសួរ %s បាន", "こんにちは%s", "你好%s", "안녕하세요  %s");
-    private static final String ANSWER_JOIN = String.join("|", ANSWERS);
-    private static final String DELIMITER_PATTERN = "(_|__|-|" + ANSWER_JOIN + ")";
+    private static final int TIMEOUT = 50;
 
     /**
      * Get UTF-8 data from {@link DatagramPacket}
@@ -83,15 +71,34 @@ public class HelloUDPClient implements HelloClient {
                 """);
     }
 
+    private static boolean checkFail(final boolean bool, final String error) {
+        if (bool) {
+            System.err.println(error);
+            return true;
+        }
+        return false;
+    }
+
+    private static List<Integer> parseIntsFromString(final String string) {
+        final List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isDigit(string.charAt(i))) {
+                final int numberBeginIndex = i;
+
+                while (i < string.length() && Character.isDigit(string.charAt(i)) ) {
+                    i++;
+                }
+
+                final String numberString = string.substring(numberBeginIndex, i);
+                ans.add(Integer.parseInt(numberString));
+            }
+        }
+
+        return ans;
+    }
 
     @Override
     public void run(final String host, final int port, final String prefix, final int threads, final int requests) {
-        final String prefixPattern = "(" + prefix
-                + "|" + prefix.replaceAll("(.)", "$1$1")
-                + "|" + "_".repeat(prefix.length())
-                + "|" + "-".repeat(prefix.length())
-                + "|" + ANSWER_JOIN + ")";
-
         final InetAddress address;
         final AtomicReference<RuntimeException> exception = new AtomicReference<>(null);
         try {
@@ -100,10 +107,10 @@ public class HelloUDPClient implements HelloClient {
             throw new RuntimeException(e);
         }
         final Thread mainThread = Thread.currentThread();
-        final var threadsList = IntStream.range(0, threads)
+        final var threadsList = IntStream.range(1, threads + 1)
                 .mapToObj(threadNum -> new Thread(() ->
                         threadAction(port, (requestNum) -> prefix + threadNum + "_" + requestNum,
-                                requests, prefixPattern, address, exception, threadNum,
+                                requests, address, exception, threadNum,
                                 mainThread))
                 )
                 .peek(Thread::start)
@@ -131,7 +138,6 @@ public class HelloUDPClient implements HelloClient {
             final int port,
             final Function<Integer, String> requestGenerator,
             final int requests,
-            final String prefixPattern,
             final InetAddress address,
             final AtomicReference<RuntimeException> exception,
             final int threadNum,
@@ -139,7 +145,7 @@ public class HelloUDPClient implements HelloClient {
     ) {
         try (final var datagramSocket = new DatagramSocket()) {
             datagramSocket.setSoTimeout(TIMEOUT);
-            for (int requestNum = 0; requestNum < requests; requestNum++) {
+            for (int requestNum = 1; requestNum < requests + 1; requestNum++) {
                 while (!Thread.interrupted()) {
                     final String request = requestGenerator.apply(requestNum);
                     final var bytes = request.getBytes(StandardCharsets.UTF_8);
@@ -150,7 +156,7 @@ public class HelloUDPClient implements HelloClient {
                                 datagramSocket.getReceiveBufferSize());
                         datagramSocket.receive(packetForReceive);
                         final var ans = getDecodedData(packetForReceive);
-                        if (validateAnswer(ans, prefixPattern,
+                        if (validateAnswer(ans,
                                 threadNum,
                                 requestNum)) {
                             System.out.println(request + " " + ans);
@@ -173,12 +179,11 @@ public class HelloUDPClient implements HelloClient {
         }
     }
 
-    private boolean validateAnswer(final String ans, final String prefixPattern, final int threadNum,
+    private boolean validateAnswer(final String ans, final int threadNum,
             final int request) {
-        return ANSWERS.stream()
-                .map(it -> it.replaceAll("%s", prefixPattern + threadNum + DELIMITER_PATTERN + request))
-                .map(Pattern::compile)
-                .map(Pattern::asMatchPredicate)
-                .anyMatch(it -> it.test(ans));
+        final var numbers = parseIntsFromString(ans);
+        return !checkFail(numbers.size() != 2, "Not two numbers in string")
+                && !checkFail(numbers.get(0) != threadNum, "First numbers isn't thread num")
+                && !checkFail(numbers.get(1) != request, "Second numbers isn't request");
     }
 }
