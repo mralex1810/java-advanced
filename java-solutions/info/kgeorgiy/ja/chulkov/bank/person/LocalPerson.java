ae1ac46ffac427ed49c89d1e7bfad5151d183c83
mralex1810 <sash39av@gmail.com>
2023-05-12 15:19:45 +0300

    11 change inh [ 1 minute ]

---
+++ java-solutions/info/kgeorgiy/ja/chulkov/bank/person/LocalPerson.java
@@ -0,0 +1,21 @@
+package info.kgeorgiy.ja.chulkov.bank.person;
+
+import info.kgeorgiy.ja.chulkov.bank.account.Account;
+
+public interface LocalPerson extends RemotePerson {
+
+    @Override
+    String getFirstName() ;
+
+    @Override
+    String getSecondName();
+
+    @Override
+    String getPassport() ;
+
+    @Override
+    Account createAccount(String id);
+
+    @Override
+    Account getAccount(String id) ;
+}
