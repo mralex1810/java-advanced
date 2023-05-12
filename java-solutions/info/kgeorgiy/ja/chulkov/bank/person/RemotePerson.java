eb17819b7aec7a63a8f51d87deebd0a336cdecdc
mralex1810 <sash39av@gmail.com>
2023-05-12 15:12:35 +0300

    11 change inh [ 6 minutes ]

--- java-solutions/info/kgeorgiy/ja/chulkov/bank/person/RemotePerson.java
+++ java-solutions/info/kgeorgiy/ja/chulkov/bank/person/RemotePerson.java
@@ -1,38 +1,6 @@
 package info.kgeorgiy.ja.chulkov.bank.person;
 
-import info.kgeorgiy.ja.chulkov.bank.account.Account;
 import java.rmi.Remote;
-import java.rmi.RemoteException;
 
-public interface RemotePerson extends Remote {
-
-    /**
-     * @return first name of this person
-     */
-    String getFirstName() throws RemoteException;
-    /**
-     * @return second name of this person
-     */
-    String getSecondName() throws RemoteException;
-    /**
-     * @return passport of this person
-     */
-    String getPassport() throws RemoteException;
-
-    /**
-     * Creates a new account with specified identifier if it does not already exist.
-     *
-     * @param id account id
-     * @return created or existing account.
-     */
-    Account createAccount(String id) throws RemoteException;
-
-    /**
-     * Returns account by identifier.
-     *
-     * @param id account id
-     * @return account with specified identifier or {@code null} if such account does not exist.
-     */
-    Account getAccount(String id) throws RemoteException;
-
+public interface RemotePerson extends Person, Remote {
 }
